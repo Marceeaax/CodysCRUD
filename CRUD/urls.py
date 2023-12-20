6d59fbdeebe05487637fe50ea9c3195c88b991f8
marceeaax <marcelo.adrian2011@fpuna.edu.py>
2023-12-20 10:58:55 -0300

    modificando el grid y ajustando datos

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -24,3 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('persona.urls')),  # Asegúrate de que esto esté correcto
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- persona/models.py
+++ persona/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from PIL import Image
 
 class Usuario(models.Model):
     foto_perfil = models.ImageField(upload_to='fotos_perfiles/')
@@ -7,5 +8,13 @@ class Usuario(models.Model):
     telefono = models.CharField(max_length=20)
     fecha_nacimiento = models.DateField()
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.foto_perfil.path)
+        output_size = (100, 100)  # Tamaño deseado
+        img.thumbnail(output_size)
+        img.save(self.foto_perfil.path)
+
     def __str__(self):
         return self.nombre_apellido
+

ab36168fd1481e8ec0f9b11999fc50ab52d726fa
marceeaax <marcelo.adrian2011@fpuna.edu.py>
2023-12-20 11:38:18 -0300

    agregar settings

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'persona'
 ]
 
@@ -125,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'

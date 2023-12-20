d7f3564ef8416fd20187c838d67a27475b8049aa
marceeaax <marcelo.adrian2011@fpuna.edu.py>
2023-12-20 09:17:23 -0300

    agregando vistas, formularios, modelos, configuraciones y requerimientos

--- persona/models.py
+++ persona/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    foto_perfil = models.ImageField(upload_to='fotos_perfiles/')
+    nombre_apellido = models.CharField(max_length=100)
+    correo_electronico = models.EmailField()
+    telefono = models.CharField(max_length=20)
+    fecha_nacimiento = models.DateField()
+
+    def __str__(self):
+        return self.nombre_apellido
--- persona/views.py
+++ persona/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Usuario
+from .forms import UsuarioForm
+
+def lista_usuarios(request):
+    usuarios = Usuario.objects.all()
+    return render(request, 'usuarios/lista.html', {'usuarios': usuarios})
+
+def crear_usuario(request):
+    if request.method == "POST":
+        form = UsuarioForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_usuarios')
+    else:
+        form = UsuarioForm()
+    return render(request, 'usuarios/form.html', {'form': form})
+
+def editar_usuario(request, pk):
+    usuario = Usuario.objects.get(id=pk)
+    if request.method == "POST":
+        form = UsuarioForm(request.POST, request.FILES, instance=usuario)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_usuarios')
+    else:
+        form = UsuarioForm(instance=usuario)
+    return render(request, 'usuarios/form.html', {'form': form})
+
+def eliminar_usuario(request, pk):
+    Usuario.objects.get(id=pk).delete()
+    return redirect('lista_usuarios')
